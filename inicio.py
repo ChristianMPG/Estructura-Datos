90e57b71498053c134986faa84153cbcc27e50d9
mateogonzalez0301 <mateogonzalez0301@gmail.com>
2024-08-10 17:24:41 +0000

    Se creo un codigo nuevo

--- inicio.py
+++ inicio.py
@@ -1,2 +1,6 @@
-edad = 22
-print edad
+edad = int(input("Introduce tu edad: "))  # Solicitar la edad al usuario
+
+if edad < 18:
+    print("No puedes entrar.")  # Mensaje si es menor de 18
+else:
+    print("Puedes entrar.")  # Mensaje si es mayor o igual a 18
